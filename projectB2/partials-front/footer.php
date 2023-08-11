fa04c268a8188efaba3f8639bb92912c834826b6
Lambade Pritam <113450308+pritamlambade@users.noreply.github.com>
2023-08-11 18:40:28 +0530

    Add files via upload

---
+++ projectB2/partials-front/footer.php
@@ -0,0 +1,35 @@
+<!-- social Section Starts Here -->
+    <section class="footer" style="background-color:#acdef3;">
+    <section class="social" >
+        <div class="container text-center"data-aos="zoom-out-up">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer" style="background-color:#acdef3;">
+        <div class="container text-center">
+            <p>© 2022 All rights reserved. Quickyy Foods , Designed By <a href="#">TE Group</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script>
+  AOS.init();
+</script>
+
+</html>
---
+++ projectB2/partials-front/menu.php
@@ -0,0 +1,48 @@
+<?php include('config/constants.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quickyy Foods</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar" style="background-color: black;">
+        <div class="container">
+            <div class="logo">
+                <a href="#" title="Logo">
+                    
+                    <img src="images/logo2.jpg" alt="Restaurant Logo" class="img-responsive img-border" style ="border-radius:40px;">
+                    
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>">Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>categories.php">Categories</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>foods.php">Foods</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>about_us.php">About us</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
